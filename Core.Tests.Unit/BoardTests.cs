f9d55838a1f0dc19e84a3b5631e8ba84674ea45d
Tomasz Pamula <tomasz.p.pamula@gmail.com>
2014-11-16 20:58:42 +0100

    Additional test.

--- Core.Tests.Unit/BoardTests.cs
+++ Core.Tests.Unit/BoardTests.cs
@@ -8,6 +8,17 @@ namespace Core.Tests.Unit
     public class BoardTests
     {
         [Fact]
+        public void should_become_alive_with_three_neighbors()
+        {
+            var board = new Board();
+            board[0, 1] = board[0, 2] = board[0, 3] = CellType.Alive;
+
+            board.NextGeneration();
+
+            board[1, 2].ShouldBe(CellType.Alive);
+        }
+
+        [Fact]
         public void should_die_if_less_than_two_neighbors()
         {
             var board = new Board();
@@ -19,14 +30,25 @@ namespace Core.Tests.Unit
         }
 
         [Fact]
-        public void should_stay_alive_with_two_neighbors()
+        public void should_die_with_four_neighbors()
         {
             var board = new Board();
-            board[0, 1] = board[0, 2] = board[1, 1] = CellType.Alive;
+            board[0, 1] = board[0, 2] = board[0, 3] = board[1, 1] = board[1, 2] = CellType.Alive;
 
             board.NextGeneration();
 
-            board[1, 1].ShouldBe(CellType.Alive);
+            board[1, 2].ShouldBe(CellType.Dead);
+        }
+
+        [Fact]
+        public void should_die_with_seven_neighbors()
+        {
+            var board = new Board();
+            board[0, 1] = board[0, 2] = board[0, 3] = board[1, 1] = board[1, 2] = board[1, 3] = board[2, 1] = board[2, 2] = CellType.Alive;
+
+            board.NextGeneration();
+
+            board[1, 2].ShouldBe(CellType.Dead);
         }
 
         [Fact]
@@ -41,25 +63,14 @@ namespace Core.Tests.Unit
         }
 
         [Fact]
-        public void should_die_with_four_neighbors()
+        public void should_stay_alive_with_two_neighbors()
         {
             var board = new Board();
-            board[0, 1] = board[0, 2] = board[0, 3] = board[1, 1] = board[2, 1] = CellType.Alive;
+            board[0, 1] = board[0, 2] = board[1, 1] = CellType.Alive;
 
             board.NextGeneration();
 
-            board[2, 1].ShouldBe(CellType.Dead);
-        }
-
-        [Fact]
-        public void should_become_alive_with_three_neighbors()
-        {
-            var board = new Board();
-            board[0, 1] = board[0, 2] = board[0, 3] = CellType.Alive;
-
-            board.NextGeneration();
-
-            board[1, 2].ShouldBe(CellType.Alive);
+            board[1, 1].ShouldBe(CellType.Alive);
         }
     }
 }
